d6aed0dc205e9d2acfcb3e63f687ce02df1e1ac6
merve-dagli <80669348+merve-dagli@users.noreply.github.com>
2021-04-27 03:18:59 +0300

    15.gün

---
+++ Core/CrossCuttingConcerns/Cashing/ICacheManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Cashing
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+        object Get(string key);
+        void Add(string key, object value,int duration);
+        bool IsAdd(string key);//cash de var mı data?
+        void Remove(string key);
+        void RemoveByPattern(string pattern);
+    }
+}
